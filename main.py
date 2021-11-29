1b39d83d48ccbd91f59b748789d307367e8237b7
stanzi1626 <stansfield_alex@yahoo.com>
2021-11-29 21:19:21 +0000

    Plotted data and have general function Scipy.optimize cannot find parameters? Maybe make a 2D hill climbing algorithm

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from numpy.core.defchararray import splitlines
+import scipy.optimize
 
 FILE_NAME1 = 'z_boson_data_1.csv'
 FILE_NAME2 = 'z_boson_data_2.csv'
+
+speed_of_light = 299792458 #m/s
+gamma_ee = 0.08391 #GeV
+'''gamma_z = 3 #Gev
+m_z = 90 #Gev/c^2''' #values should be around these
+
+def general_function(E, m, gamma):
+    """
+    Takes in energy values and returns the cross-sectional area
+
+    Parameters
+    ----------
+    E: array
+    m_z: float
+    gamma_z: float
+
+    Returns
+    -------
+    2D numpy array of floats    
+    """
+    conversion = 0.3894e6 #change the values into nb
+
+    return (12*math.pi/(m**2))*(np.square(E)/((np.square(E) - m**2)**2 + (m**2*gamma**2))) * gamma_ee**2 * conversion
+
+def find_parameters(data):
+    x, y = scipy.optimize.curve_fit(general_function, data[:,0], data[:,1], sigma = data[:, 2])
+    return x[0], x[1]
 
 def read_data(filname):
     """
@@ -18,38 +45,21 @@ def read_data(filname):
     -------
     2D numpy array of floats
     """
-    try:
-        input_file = open(filname, 'r')
-        DATA_OPEN = True
+    return np.genfromtxt(filname, dtype='float', delimiter=',', skip_header=1)
 
-    except FileNotFoundError:
-        print('Unable to open file.')
+def filter(data):
+    index = 0
+    for line in data:
+        for i in range(0, len(line)):
+            if np.isnan(line[i]):
+                data = np.delete(data, index, axis=0)
+                index -= 1
+                break
+        index += 1
+    return data
 
-    if DATA_OPEN:
 
-        data_array = np.zeros((0, 3))
-        SKIPPED_FIRST_LINE = False
-        for line in input_file:
-
-            if not SKIPPED_FIRST_LINE:
-                SKIPPED_FIRST_LINE = True
-
-            else:
-
-                split_up = line.split(',')
-                try:
-                    temp = np.array([float(split_up[0]), float(split_up[1]), float(split_up[2])])
-                    if np.isnan(temp[0]) or np.isnan(temp[1]) or np.isnan(temp[2]):
-                        print('yikes')
-                    else:    
-                        data_array = np.vstack((data_array, temp))
-                except ValueError:
-                    pass
-    input_file.close()
-
-    return data_array
-
-def plot_data(data1, data2):
+def plot_data(data):
     """
     Produces a plot of the data.
 
@@ -64,20 +74,18 @@ def plot_data(data1, data2):
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(data1[:, 0], data1[:, 1], marker='o', s=4)
-    ax.scatter(data2[:, 0], data2[:, 1], marker='o', s=4)
+    ax.scatter(data[:, 0], data[:, 1], marker='o', s=4)
     ax.set_ylim(0, 2.5)
     ax.set_title('Plot of data')
+    '''ax.scatter(data[:,0], general_function(data[:,0], m_z, gamma_z))'''
     plt.show()
 
     return None
 
-
 def main():
-    pass
-    data1 = read_data(FILE_NAME1)
-    data2 = read_data(FILE_NAME2)
-    plot_data(data1, data2)
+    data = np.vstack((filter(read_data(FILE_NAME1)),filter(read_data(FILE_NAME2))))
+    '''expected_m_z, expected_gamma_z = find_parameters(data)'''
+    '''plot_data(data, expected_m_z, expected_gamma_z)'''
 
     return 0
 
