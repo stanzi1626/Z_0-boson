72fa1886a339e0a45411cd48bf23ed82d2c23de4
stanzi1626 <stansfield_alex@yahoo.com>
2021-12-02 20:18:42 +0000

    Working 2d hillclimbing function

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import scipy.optimize
 FILE_NAME1 = 'z_boson_data_1.csv'
 FILE_NAME2 = 'z_boson_data_2.csv'
 
+#line 18 data 1 86.5354,1.1222,0.0754
+
 gamma_ee = 0.08391 #GeV
 start_gamma_z = 3 #Gev
 start_m_z = 90 #Gev/c^2 #values should be around these
-STEP_SIZE = 0.0001
-TOLERANCE = 0.0001
-MAX_ITERATIONS = 100000
+STEP_SIZE = 0.1
+TOLERANCE = 0.000001
+MAX_ITERATIONS = 10000
 
 def general_function(E, m, gamma):
     """
@@ -31,7 +33,11 @@ def general_function(E, m, gamma):
 
     return (12*math.pi/(m**2))*(np.square(E)/((np.square(E) - m**2)**2 + (m**2*gamma**2))) * gamma_ee**2 * conversion
 
-def hill_climbing(function, x_minimum, step=STEP_SIZE):
+
+#the idea at the momement is to do each parameter one at a time until the chi-sqauared is within some tolerance, this 
+#is a work around while the scipy.optimize isnt working
+
+def hill_climbing(function, m_minimum, gamma_minimum, step1=STEP_SIZE, step2=STEP_SIZE):
     """
     Performs 1D hill climbing algorithm with varying step size.
 
@@ -53,31 +59,64 @@ def hill_climbing(function, x_minimum, step=STEP_SIZE):
         Number of iterations
     """
     difference = 1
-    minimum = function(x_minimum)
+    minimum = function(m_minimum, gamma_minimum)
     counter = 0
+    counter_1 = 0
+    counter_2 = 0
 
     while difference > TOLERANCE:
         counter += 1
-        minimum_test_minus = function(x_minimum - step)
-        minimum_test_plus = function(x_minimum + step)
-        if minimum_test_minus < minimum:
-            x_minimum -= step
-            difference = minimum - minimum_test_minus
-            minimum = function(x_minimum)
-        elif function(x_minimum + step) < minimum:
-            x_minimum += step
-            difference = minimum - minimum_test_plus
-            minimum = function(x_minimum)
-        else:
-            step = step * 0.1
+
+        while True:
+            counter_1 += 1
+            minimum1_test_minus = function(m_minimum - step1, gamma_minimum)
+            minimum1_test_plus = function(m_minimum + step1, gamma_minimum)
+            if minimum1_test_minus < minimum:
+                m_minimum -= step1
+                difference = minimum - minimum1_test_minus
+                minimum = function(m_minimum, gamma_minimum)  
+                break   
+            elif minimum1_test_plus < minimum:
+                m_minimum += step1
+                difference = minimum - minimum1_test_plus
+                minimum = function(m_minimum, gamma_minimum)
+                break
+            else:
+                step1 = step1 * 0.1
+            if counter_1 == 1000:
+                break
+
+        while True:
+            counter_2 += 1
+            minimum2_test_minus = function(m_minimum, gamma_minimum - step2)
+            minimum2_test_plus = function(m_minimum, gamma_minimum + step2)
+            if minimum2_test_minus < minimum:
+                gamma_minimum -= step2
+                difference = minimum - minimum2_test_minus
+                minimum = function(m_minimum, gamma_minimum)
+                break
+            elif minimum2_test_plus < minimum:
+                gamma_minimum += step2
+                difference = minimum - minimum2_test_plus
+                minimum = function(m_minimum, gamma_minimum)
+                break
+            else:
+                step2 = step2 * 0.1
+            if counter_2 == 1000:
+                break
 
         if counter == MAX_ITERATIONS:
-            print('Failed to find solution after {0:d} iterations.'.
+            print('Failed to find best solution after {0:d} iterations.'.
                   format(counter))
             break
 
-    return x_minimum, minimum, counter
+    return m_minimum, gamma_minimum, minimum
 
+def chi_square(observation, observation_uncertainty, prediction):
+    """
+    Returns the chi sqaured
+    """
+    return np.sum((observation - prediction)**2 / observation_uncertainty**2)
 
 def find_parameters(data):
     """
@@ -102,20 +141,29 @@ def read_data(filname):
 
 def filter(data):
     """
-    
+    removes all nans
+    removes all 0 uncertainties
+    removes values that are 10* more than the average without it 
     """
     index = 0
+    average = np.average(data[:,1])
     for line in data:
         for i in range(0, len(line)):
             if np.isnan(line[i]):
                 data = np.delete(data, index, axis=0)
                 index -= 1
                 break
+        if line[2] == 0:
+            data = np.delete(data, index, axis=0)
+            index -= 1
+        if np.abs(((average - line[1]/len(data[:,1]))) - average) > 0.1:
+            data = np.delete(data, index, axis=0)
+            index -= 1
         index += 1
     return data
 
 
-def plot_data(data):
+def plot_data(data, m_z, gamma_z):
     """
     Produces a plot of the data.
 
@@ -134,16 +182,18 @@ def plot_data(data):
     ax.set_ylim(0, 2.5)
     ax.set_title('Plot of data')
     ax.scatter(data[:,0], general_function(data[:,0], start_m_z, start_gamma_z))
+    ax.scatter(data[:,0], general_function(data[:,0], m_z, gamma_z))
     plt.show()
 
     return None
 
 def main():
     data = np.vstack((filter(read_data(FILE_NAME1)),filter(read_data(FILE_NAME2))))
-    expected_m_z, expected_gamma_z = find_parameters(data)
-    print(expected_gamma_z)
+    expected_m_z, expected_gamma_z, chi = hill_climbing(lambda coefficient1, coefficient2: chi_square(data[:,1], data[:,2], general_function(data[:,0], coefficient1, coefficient2)), start_m_z, start_gamma_z)
     print(expected_m_z)
-    '''plot_data(data)'''
+    print(expected_gamma_z)
+    print(chi)
+    plot_data(data, expected_m_z, expected_gamma_z)
 
     return 0
 
